5fdea85f59b6f70cb581cd08b26645431d673950
laazyCmd <laazy.pr00gramming@protonmail.com>
2022-02-09 08:39:55 +0800

    🎨 STRUC: minor adjustments to Database.java for clarity

--- src/main/java/com/project/crud/database/Database.java
+++ src/main/java/com/project/crud/database/Database.java
@@ -20,8 +20,8 @@ public class Database {
 
             System.out.println( "Successfully connected to the database!" );
             return DriverManager.getConnection( url, username, password );
-        } catch ( SQLException err ) {
-            System.err.println( "Warning! SQLException has occurred in getConnection() function: " + err.getMessage() );
+        } catch ( Exception err ) {
+            System.err.println( "Warning! An exception has occurred in getConnection() method: " + err.getMessage() );
         }
 
         return null;
@@ -30,9 +30,9 @@ public class Database {
     public List< Student > getStudents() {
         List< Student > list = new ArrayList<>();
 
-        try ( final Connection con = getConnection() ) {
-            final String command = "SELECT * FROM `students`";
-            final PreparedStatement getStudents = con.prepareStatement( command );
+        try ( final Connection conn = getConnection() ) {
+            final String query = "SELECT * FROM `students`";
+            final PreparedStatement getStudents = conn.prepareStatement( query );
             final ResultSet response = getStudents.executeQuery();
 
             while ( response.next() ) {
@@ -47,70 +47,70 @@ public class Database {
                         response.getString( 8 ) )
                 );
             }
-        } catch ( SQLException err ) {
-            System.err.println( "Warning! SQLException has occurred in getStudents() function: " + err.getMessage() );
+        } catch ( Exception err ) {
+            System.err.println( "Warning! An exception has occurred in getStudents() method: " + err.getMessage() );
         }
 
         return list;
     }
 
     public void createStudentsTable() {
-        try ( final Connection con = getConnection() ) {
-            final PreparedStatement createTable = con.prepareStatement( "create table if not exists students ( studentNo int not null, firstName char(32) not null, lastName char(32) not null, yearLevel int not null, age int not null, gender char(10) not null, program char(64) not null, imagePath char(255) not null )" );
+        try ( final Connection conn = getConnection() ) {
+            final PreparedStatement createTable = conn.prepareStatement( "create table if not exists students ( studentNo int not null, firstName char(32) not null, lastName char(32) not null, yearLevel int not null, age int not null, gender char(10) not null, program char(64) not null, imagePath char(255) not null )" );
             final int tableExists = createTable.executeUpdate();
 
             if ( tableExists == 0 ) System.out.println( "A `students` table already exists. Skipping..." );
-                else System.out.println( "A `students` table doesn't exist. Successfully created one!" );
-        } catch ( SQLException err ) {
-            System.err.println( "Warning! SQLException has occurred in createStudentsTable() function: " + err.getMessage() );
+            else System.out.println( "A `students` table doesn't exist. Successfully created one!" );
+        } catch ( Exception err ) {
+            System.err.println( "Warning! An exception has occurred in createStudentsTable() method: " + err.getMessage() );
         }
     }
 
     public void insertStudent( int studentNumber, String firstName, String lastName, int yearLevel, int age, String gender, String program, String imagePath ) {
-        try ( final Connection con = getConnection() ) {
-            final String command = String.format( "insert into students ( studentNo, firstName, lastName, yearLevel, age, gender, program, imagePath ) values ( %d, '%s', '%s', %d, %d, '%s', '%s', '%s' )", studentNumber, firstName, lastName, yearLevel, age, gender, program, imagePath );
-            final PreparedStatement insertStudent = con.prepareStatement( command );
+        try ( final Connection conn = getConnection() ) {
+            final String query = String.format( "insert into students ( studentNo, firstName, lastName, yearLevel, age, gender, program, imagePath ) values ( %d, '%s', '%s', %d, %d, '%s', '%s', '%s' )", studentNumber, firstName, lastName, yearLevel, age, gender, program, imagePath );
+            final PreparedStatement insertStudent = conn.prepareStatement( query );
             final int successful = insertStudent.executeUpdate();
 
             if ( successful == 1 ) System.out.println( "Successfully added a new entry into the `students` table." );
-                else System.err.println( "Something went wrong! You should check it out, maybe it's the database... or you!" );
-        } catch ( SQLException err ) {
-            System.err.println( "Warning! SQLException has occurred in insertStudent() function: " + err.getMessage() );
+            else System.err.println( "Something went wrong! You should check it out, maybe it's the database... or you!" );
+        } catch ( Exception err ) {
+            System.err.println( "Warning! An exception has occurred in insertStudent() method: " + err.getMessage() );
         }
     }
 
     public void updateStudent( int studentNumber, String updatedFirstName, String updatedLastName, int updatedYearLevel, int updatedAge, String updatedGender, String updatedProgram, String updatedImagePath ) {
-        try ( final Connection con = getConnection() ) {
-            final String command = String.format( "update `students` set firstName = '%s', lastName = '%s', yearLevel = %d, age = %d, gender = '%s', program = '%s', imagePath = '%s' where studentNo = %d", updatedFirstName, updatedLastName, updatedYearLevel, updatedAge, updatedGender, updatedProgram, updatedImagePath, studentNumber  );
-            final PreparedStatement updateStudent = con.prepareStatement( command );
+        try ( final Connection conn = getConnection() ) {
+            final String query = String.format( "update `students` set firstName = '%s', lastName = '%s', yearLevel = %d, age = %d, gender = '%s', program = '%s', imagePath = '%s' where studentNo = %d", updatedFirstName, updatedLastName, updatedYearLevel, updatedAge, updatedGender, updatedProgram, updatedImagePath, studentNumber  );
+            final PreparedStatement updateStudent = conn.prepareStatement( query );
             final int successful = updateStudent.executeUpdate();
 
             if ( successful == 1 ) System.out.println( "Successfully updated the entry with a student number " + studentNumber + " into the `students` table." );
             else System.err.println( "Something went wrong! You should check it out, maybe it's the database... or you!" );
-        } catch ( SQLException err ) {
-            System.err.println( "Warning! SQLException has occurred in updateStudent() function: " + err.getMessage() );
+        } catch ( Exception err ) {
+            System.err.println( "Warning! An exception has occurred in updateStudent() method: " + err.getMessage() );
         }
     }
 
     public void removeStudent( int studentNumber ) {
-        try ( final Connection con = getConnection() ) {
-            final String command = "delete from `students` where studentNo = " + studentNumber;
-            final PreparedStatement removeStudent = con.prepareStatement( command );
+        try ( final Connection conn = getConnection() ) {
+            final String query = "delete from `students` where studentNo = " + studentNumber;
+            final PreparedStatement removeStudent = conn.prepareStatement( query );
             final int successful = removeStudent.executeUpdate();
 
             if ( successful == 1 ) System.out.println( "Successfully removed the entry with a student number " + studentNumber + " from the `students` table." );
             else System.err.println( "Something went wrong! You should check it out, maybe it's the database... or you!" );
-        } catch ( SQLException err ) {
-            System.err.println( "Warning! SQLException has occurred in removeStudent() function: " + err.getMessage() );
+        } catch ( Exception err ) {
+            System.err.println( "Warning! An exception has occurred in removeStudent() method: " + err.getMessage() );
         }
     }
 
     public List< Student > searchStudents( String keywords ) {
         List< Student > searchedStudents = new ArrayList<>();
 
-        try ( final Connection con = getConnection() ) {
-            final String command = "select * from `students` where concat( studentNo, firstName, lastName, program ) REGEXP '" + keywords + "'";
-            final PreparedStatement searchStudents = con.prepareStatement( command );
+        try ( final Connection conn = getConnection() ) {
+            final String query = "select * from `students` where concat( studentNo, firstName, lastName, program ) REGEXP '" + keywords + "'";
+            final PreparedStatement searchStudents = conn.prepareStatement( query );
             final ResultSet response = searchStudents.executeQuery();
 
             while ( response.next() ) {
@@ -125,22 +125,22 @@ public class Database {
                         response.getString( 8 ) )
                 );
             }
-        } catch ( SQLException err ) {
-            System.err.println( "Warning! SQLException has occurred in searchStudents() function: " + err.getMessage() );
+        } catch ( Exception err ) {
+            System.err.println( "Warning! An exception has occurred in searchStudents() method: " + err.getMessage() );
         }
 
         return searchedStudents;
     }
 
     public boolean checkIfDuplicate( int studentNumber ) {
-        try ( final Connection con = getConnection() ){
-            final String command = String.format( "select * from `students` where `studentNo` like %d", studentNumber );
-            final PreparedStatement checkDuplicate = con.prepareStatement( command );
+        try ( final Connection conn = getConnection() ){
+            final String query = String.format( "select * from `students` where `studentNo` like %d", studentNumber );
+            final PreparedStatement checkDuplicate = conn.prepareStatement( query );
             final ResultSet hasDuplicate = checkDuplicate.executeQuery();
 
             return hasDuplicate.next();
-        } catch ( SQLException err ) {
-            System.err.println( "Warning! SQLException has occurred in checkIfDuplicate() function: " + err.getMessage() );
+        } catch ( Exception err ) {
+            System.err.println( "Warning! An exception has occurred in checkIfDuplicate() method: " + err.getMessage() );
         }
 
         return false;
